e4339154232750c77acf1ef9fc8a2c66b4a9de9f
Oscar Cederberg <44801604+oscarcederberg@users.noreply.github.com>
2023-09-12 13:15:26 +0200

    Fix Moore

--- lab_1_vivado_project/lab_1_vivado_project.srcs/sources_1/imports/src/sequence_detector_moore.vhd
+++ lab_1_vivado_project/lab_1_vivado_project.srcs/sources_1/imports/src/sequence_detector_moore.vhd
@@ -12,21 +12,16 @@ entity sequence_detector_moore is
 end sequence_detector_moore;
 
 architecture behavioural of sequence_detector_moore is
-
-
     -- Define a enumeration type for the states
     type state_type is (s_init, s_1, s_10, s_101, s_1010);
-    
     -- Define the needed internal signals
     signal current_state, next_state: state_type;
-
-begin
-            
+begin  
     -- purpose: Implements the registers for the sequence decoder
     -- type : sequential
-    registers: process (clk, reset_n)
+    registers: process (clk, reset_n, data_valid)
     begin
-        if reset_n = '0' then
+        if reset_n = '0' or data_valid = '0' then
             current_state <= s_init;
         elsif rising_edge(clk) then
             current_state <= next_state;
@@ -35,52 +30,43 @@ begin
     
     -- purpose: Implements the next_state logic as well as the output logic
     -- type : combinational
-    combinational: process (clk) -- fill out the sensitivity list
+    combinational: process (current_state, data_serial) -- fill out the sensitivity list
     begin
         -- set default value
         data_out <= '0';
         next_state <= current_state;
-        
-        if reset_n = '0' then    
-            --current_state <= s_init;
-            
-            
-        elsif rising_edge (clk) and data_valid = '1' then
-            case current_state is
-                when s_init =>
-                     if data_serial = '0' then
-                        next_state <= s_init;
-                     else
-                        next_state <= s_1;
-                     end if;
-                when s_1 =>
-                     if data_serial = '0' then
-                        next_state <= s_10;
-                     else
-                        next_state <= s_1;
-                     end if;
-                 when s_10 =>
-                     if data_serial = '0' then
-                        next_state <= s_init;
-                     else
-                        next_state <= s_101;
-                     end if;
-                 when s_101 =>
-                     if data_serial = '0' then
-                        next_state <= s_1010;
-                     else
-                        next_state <= s_1;
-                     end if;
-                 when s_1010 =>
-                    data_out <= '1';
-                     if data_serial = '0' then
-                        next_state <= s_init;
-                     else
-                        next_state <= s_101;
-                     end if;
-             end case;
+
+        if current_state = s_init then
+             if data_serial = '0' then
+                next_state <= s_init;
+             else
+                next_state <= s_1;
+             end if;
+        elsif current_state = s_1 then
+             if data_serial = '0' then
+                next_state <= s_10;
+             else
+                next_state <= s_1;
+             end if;
+         elsif current_state = s_10 then
+             if data_serial = '0' then
+                next_state <= s_init;
+             else
+                next_state <= s_101;
+             end if;
+         elsif current_state = s_101 then
+             if data_serial = '0' then
+                next_state <= s_1010;
+             else
+                next_state <= s_1;
+             end if;
+         elsif current_state = s_1010 then
+            data_out <= '1';
+             if data_serial = '0' then
+                next_state <= s_init;
+             else
+                next_state <= s_101;
+             end if;
          end if;
     end process;
-
-    
 end behavioural;
